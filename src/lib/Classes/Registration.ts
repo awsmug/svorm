739c4006f8a017fbcc34986d49a3bb68adc1a3ae
Sven Wagener <sven@awesome.ug>
2022-07-19 12:48:32 +0200

    Removed number form registration

--- src/lib/Classes/Registration.ts
+++ src/lib/Classes/Registration.ts
@@ -1,5 +1,4 @@
 import Text from '$lib/Components/Inputs/Text.svelte';
-import Number from '$lib/Components/Inputs/Number.svelte';
 import Textarea from '$lib/Components/Inputs/Textarea.svelte';
 import Range from '$lib/Components/Inputs/Range.svelte';
 import Checkbox from '$lib/Components/Inputs/Checkbox.svelte';
@@ -25,7 +24,6 @@ export default class Registration {
     private registerDefaults() {
         this.addElement( 'text', Text );
         this.addElement( 'textarea', Textarea );
-        this.addElement( 'number', Number );
         this.addElement( 'range', Range );
         this.addElement( 'checkbox', Checkbox );
         this.addElement( 'choice-select', ChoiceSelect );
